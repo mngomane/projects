1b51b9847dde509cd5d4548787787402777cd428
Max <max.ngomane@gmail.com>
2015-03-27 16:57:30 +0100

    ft_next_pow2 added

--- 108.105.98.102.116/ft/math/srcs/ft_pow2.c
+++ 108.105.98.102.116/ft/math/srcs/ft_pow2.c
@@ -1,28 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_pow2.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mngomane <mngomane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/03/11 20:56:32 by mngomane          #+#    #+#             */
-/*   Updated: 2015/03/11 20:56:32 by mngomane         ###   ########.fr       */
+/*   Created: 2015/03/27 15:21:57 by mngomane          #+#    #+#             */
+/*   Updated: 2015/03/27 15:21:57 by mngomane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "ft_math.h"
 
-# include "ft_ctype.h"
-# include "ft_io.h"
-# include "ft_printf.h"
-# include "get_next_line.h"
-# include "ft_lib.h"
-# include "ft_list.h"
-# include "ft_math.h"
-# include "ft_memory.h"
-# include "ft_string.h"
-# include "ft_unix.h"
-# include "ft_wchar.h"
+size_t		ft_next_pow2(size_t x)
+{
+	size_t	bit;
 
-#endif
+	bit = 1;
+	x--;
+	while (bit < 8 * sizeof(long))
+	{
+		x |= x >> bit;
+		bit <<= 1;
+	}
+	x++;
+	return (x);
+}
--- 108.105.98.102.116/include/ft_math.h
+++ 108.105.98.102.116/include/ft_math.h
@@ -1,28 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_math.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mngomane <mngomane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/03/11 20:56:32 by mngomane          #+#    #+#             */
-/*   Updated: 2015/03/11 20:56:32 by mngomane         ###   ########.fr       */
+/*   Created: 2015/03/27 15:18:15 by mngomane          #+#    #+#             */
+/*   Updated: 2015/03/27 15:18:15 by mngomane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#ifndef FT_MATH_H
+# define FT_MATH_H
 
-# include "ft_ctype.h"
-# include "ft_io.h"
-# include "ft_printf.h"
-# include "get_next_line.h"
-# include "ft_lib.h"
-# include "ft_list.h"
-# include "ft_math.h"
-# include "ft_memory.h"
-# include "ft_string.h"
-# include "ft_unix.h"
-# include "ft_wchar.h"
+# include <sys/types.h>
+
+size_t		ft_next_pow2(size_t x);
 
 #endif
