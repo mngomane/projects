81334068169123a2e04886985c550f510aa2c034
Max <max.ngomane@gmail.com>
2015-03-30 00:49:01 +0200

    -r added

--- 108.115/108.115/srcs/base.c
+++ 108.115/108.115/srcs/base.c
@@ -63,6 +63,20 @@ static t_list	*file_part(t_list *lst, u_char flags, u_char *check)
 	return (lst);
 }
 
+/*static t_lut	*init_lookup(void)
+{
+	t_lut		*lookup;
+	int			tab[8];
+
+	tab[0] = 0x01;
+	tab[1] = 0x02;
+	tab[2] = 0x04;
+	tab[3] = 0x04;
+	lookup = (t_lut *)ft_memalloc(sizeof(t_lut) << 3);
+	lookup[0].key = ""
+	return (lookup);
+}*/
+
 static t_list	*finit(int ac, char **av, u_char flags)
 {
 	t_list		*lst;
@@ -70,7 +84,10 @@ static t_list	*finit(int ac, char **av, u_char flags)
 
 	(void)ac, (void)av, (void)flags;
 	lst = (void *)0;
-	cmp = lexical_cmp;
+	if (F_REVERSE(flags))
+		cmp = reverse_cmp;
+	else
+		cmp = lexical_cmp;
 	sort_lstadd((void *)0, (void *)0, (void *)0);
 	while (ac--)
 		sort_lstadd(&lst, ft_lstnew(new_file("", av[ac]), sizeof(t_file)),
@@ -84,10 +101,13 @@ int				fdisplay(int ac, char **av, char *name, u_char flags)
 	u_char		check;
 	int			(*cmp)(t_list *, t_list *);
 
-	cmp = lexical_cmp;
+	if (F_REVERSE(flags))
+		cmp = reverse_cmp;
+	else
+		cmp = lexical_cmp;
 	if (ac < 1)
 	{
-		if ((lst = ilst(name, lexical_cmp)) == (void *)0)
+		if ((lst = ilst(name, cmp)) == (void *)0)
 			return (-1);
 		print_lst(lst, flags);
 		ft_lstdel(&lst, del_lst);
